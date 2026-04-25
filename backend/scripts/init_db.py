6b556a4cd7e17781595900b77c1a9529134e355e
pangeding <15533858+pangeding@user.noreply.gitee.com>
2026-04-26 00:33:54 +0800

    middle: 没有解决db问题，拉完了

--- backend/scripts/init_db.py
+++ backend/scripts/init_db.py
@@ -6,7 +6,7 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "backend"))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 from app.db.models import create_tables
 from app.db.base import db
