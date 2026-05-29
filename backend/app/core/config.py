65b552a93164ec9b62fb3cde3a5f9a95e488fb34
pangeding <15533858+pangeding@user.noreply.gitee.com>
2026-05-29 13:22:39 +0800

    fix: the wrong config fix should be stepped back

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 import os
 
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), '..', '.env'))
+load_dotenv()
 
 class Settings(BaseSettings):
     """应用配置"""
